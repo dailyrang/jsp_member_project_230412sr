bfc8e5c525242734a204eccd1f942b85a72c160f
dailyrang <69h9@naver.com>
2023-04-12 12:43:53 +0900

    회원탈퇴 기능 추가

--- src/main/java/com/dailyrang/test/MemberDAO.java
+++ src/main/java/com/dailyrang/test/MemberDAO.java
@@ -9,14 +9,14 @@ public class MemberDAO {
 	private String username = "root";
 	private String password = "1234";
 	
-	public int insertMember(MemberDTO dto)  {
+	public int insertMember(MemberDTO dto) {
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		
 		String sql = "INSERT INTO members(id, pass, email) VALUES (?,?,?)";
 		
-		int  dbFlag = 0;
+		int dbFlag = 0;
 		
 		try {
 			Class.forName(driverName);
@@ -28,22 +28,110 @@ public class MemberDAO {
 			pstmt.setString(3, dto.getEmail());			
 			
 			dbFlag = pstmt.executeUpdate();
-				
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				if(pstmt != null) {
-				pstmt.close();
-			}
-			if(conn != null) {
-				conn.close();
-			}
-			}catch(Exception e) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(Exception e) {
 				e.printStackTrace();
-			}		
+			}			
 		}
-			return dbFlag;
-		}
+		
+		return dbFlag;
+		
+	}
 	
+	public int loginCheck(String id, String pass) {
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "SELECT * FROM members WHERE id = ? AND pass = ?";
+		
+		int dbFlag = 0;
+		
+		try {
+			Class.forName(driverName);
+			conn = DriverManager.getConnection(url, username, password);
+			
+			pstmt = conn.prepareStatement(sql);				
+			pstmt.setString(1, id);
+			pstmt.setString(2, pass);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dbFlag = 1;//로그인 성공				
+			} else {
+				dbFlag = 0;//로그인 실패
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}			
+		}
+		
+		return dbFlag;
+	}
+	
+	public int deleteMember(String id) {
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		String sql = "DELETE FROM members WHERE id=?";
+		
+		int dbFlag = 0;
+		
+		try {
+			Class.forName(driverName);
+			conn = DriverManager.getConnection(url, username, password);
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);				
+			
+			dbFlag = pstmt.executeUpdate();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}			
+		}
+		
+		return dbFlag;
+		
+	}
+	
+
 }
+
